d04b9d40cf4008636b3834c48fc0ecfaf5b08a8b
Anwesh43 <anweshthecool0@gmail.com>
2019-11-09 16:52:24 +0530

    adding BoxBouncyTaker class

--- boxbouncytakerview/src/main/java/com/anwesh/uiprojects/boxbouncytakerview/BoxBouncyTakerView.kt
+++ boxbouncytakerview/src/main/java/com/anwesh/uiprojects/boxbouncytakerview/BoxBouncyTakerView.kt
@@ -162,4 +162,28 @@ class BoxBouncyTakerView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BoxBouncyTaker(var i : Int) {
+
+        private val root : BBTNode = BBTNode(0)
+        private var curr : BBTNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
