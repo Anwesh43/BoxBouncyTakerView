6604a0a506d83beb3620f63a1c1cf9896deea276
Anwesh43 <anweshthecool0@gmail.com>
2019-11-09 16:25:10 +0530

    adding constants and libraries

--- boxbouncytakerview/src/main/java/com/anwesh/uiprojects/boxbouncytakerview/BoxBouncyTakerView.kt
+++ boxbouncytakerview/src/main/java/com/anwesh/uiprojects/boxbouncytakerview/BoxBouncyTakerView.kt
@@ -4,6 +4,23 @@ package com.anwesh.uiprojects.boxbouncytakerview
  * Created by anweshmishra on 09/11/19.
  */
 
+import android.view.MotionEvent
+import android.view.View
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+
+val nodes : Int = 5
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val boxFactor : Float = 3f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val scGap : Float = 0.02f
+val delay : Long = 30
+
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.dividrScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
